fd29a9aef24445ad79faa1d61b32703fc40ecd77
Jasper Li <lixinghe66@gmail.com>
2018-02-03 16:30:47 +0800

    split into 2 list

--- test_archiver.py
+++ test_archiver.py
@@ -2,13 +2,30 @@ import unittest
 from archiver import checkSrc
 
 class TestArchiver(unittest.TestCase):
+	def fileCheck(self, path, shouldBe):
+		if shouldBe:
+			self.assertTrue(checkSrc(path))
+		else:
+			self.assertFalse(checkSrc(path))
+	def fileListCheck(self, paths, shouldBe):
+		if shouldBe:
+			for path in paths:
+				self.assertTrue(checkSrc(path))
+		else:
+			for path in paths:
+				self.assertFalse(checkSrc(path))
+
 	def testCheckPath(self):
-		path="archiver.py"
-		nopath="archiver-2.py"
-		ok = "test/1.img"
-		self.assertFalse(checkSrc(nopath))
-		self.assertTrue(checkSrc(path))
-		self.assertTrue(checkSrc(ok))
+		oks=[
+			"test/1.img",
+			"archiver.py",
+			]
+		nos = [
+			"archiver-2.py"
+			]
+		self.fileListCheck(oks, True)
+		self.fileListCheck(nos, False)
+
 
 if __name__ == '__main__':
 	    unittest.main()
