e7280626d3a78d42a03c25b90cf1230b993213ea
Jasper Li <lixinghe66@gmail.com>
2018-02-03 17:09:44 +0800

    perform file check

--- archiver.py
+++ archiver.py
@@ -8,17 +8,32 @@ def getDstName():
 	timeStamp=time.strftime("%Y%m%d-%H%M%S")
 	dst=dstDir + srcBody + timeStamp + ".7z"
 	return dst
-
+def checkFile(path):
+	tailSets={
+		"py",
+		"c",
+		"cpp",
+		"java",
+		"h",
+		"sh",
+		"pl",
+	}
+	fileTail= (os.path.splitext(path))[1][1:]
+	res = not (fileTail in tailSets)
+	if res == False:
+		print("path:%s fileTail:%s res:%s" % (path, fileTail, res))
+	return res
 def checkSrc(path):
 	if os.path.exists(path) == False:
+		print(path + " not exits")
 		return False
 	
 	if os.path.isfile(path):
-		return True
+		return checkFile(path)
 
 def main():
 	dst=getDstName()
-	print(dst)
+	print("dst name is: " + dst)
 
 if __name__ == '__main__':
 	main()
--- test_archiver.py
+++ test_archiver.py
@@ -13,15 +13,21 @@ class TestArchiver(unittest.TestCase):
 				self.assertTrue(checkSrc(path))
 		else:
 			for path in paths:
-				self.assertFalse(checkSrc(path))
+				self.assertFalse(checkSrc(path), path + " should be False")
 
 	def testCheckPath(self):
 		oks=[
 			"test/1.img",
-			"archiver.py",
 			]
 		nos = [
 			"archiver-2.py"
+			"archiver.py",
+			"test/1.c",
+			"test/1.cpp",
+			"test/1.h",
+			"test/1.java",
+			"test/1.py",
+			"test/1.sh",
 			]
 		self.fileListCheck(oks, True)
 		self.fileListCheck(nos, False)
