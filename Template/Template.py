c096bddbb533e9f259ecf4b07f96567f67422ab3
JasperLi <lixinghe66@gmail.com>
2019-05-10 20:54:12 +0800

    update template

---
+++ Template/Template.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+
+# import standard lib
+import os
+import sys
+from pdb import set_trace
+
+# import custom lib
+
+def main():
+    print("Not implement yet!")
+    return 0
+
+if __name__ == '__main__':
+    result = main()
+    sys.exit(result)
--- template.py
+++
@@ -1,6 +0,0 @@
-import os
-def main():
-    print(1024)
-
-if __name__ == '__main__':
-    main()
