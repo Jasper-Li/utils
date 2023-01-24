b620b51c1a3753c9a4e3885635d11bd5aeb2ca3e
Xinghe Li <lixinghe66@gmail.com>
2023-01-24 18:16:47 +0800

    update CPP Sample

--- CppVs/CppVs/CppVs.cpp
+++ CppVs/CppVs/CppVs.cpp
@@ -1,6 +1,8 @@
 ﻿#include <tuple>
 #include <iostream>
 #include <format>
+#include <fmt/core.h>
+
 using namespace std;
 
 int main()
@@ -12,15 +14,5 @@ int main()
     for (const auto& [arg, ret]: checks) {
         cout << format("{}: {}\n", arg, ret);
     }
+    fmt::print("{:#x} {:#b}\n", 0x15, 0b1100);
 }
-
-// 运行程序: Ctrl + F5 或调试 >“开始执行(不调试)”菜单
-// 调试程序: F5 或调试 >“开始调试”菜单
-
-// 入门使用技巧: 
-//   1. 使用解决方案资源管理器窗口添加/管理文件
-//   2. 使用团队资源管理器窗口连接到源代码管理
-//   3. 使用输出窗口查看生成输出和其他消息
-//   4. 使用错误列表窗口查看错误
-//   5. 转到“项目”>“添加新项”以创建新的代码文件，或转到“项目”>“添加现有项”以将现有代码文件添加到项目
-//   6. 将来，若要再次打开此项目，请转到“文件”>“打开”>“项目”并选择 .sln 文件
